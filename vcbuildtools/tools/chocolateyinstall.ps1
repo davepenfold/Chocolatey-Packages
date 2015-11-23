92dd5b47ee93c2fd94362d626d9b725fe51029dd
Dave Penfold <davepenfold@gmail.com>
2015-11-22 21:34:15 -0800

    Initial commit

---
+++ vcbuildtools/tools/chocolateyinstall.ps1
@@ -0,0 +1,23 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName= 'vcbuildtools'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://download.microsoft.com/download/E/4/C/E4C8C3B5-7B82-4069-AD59-3A7FF8F87C74/VisualCppBuildTools_Full.exe'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+
+  silentArgs   = '/S'
+  validExitCodes= @(0)
+
+  softwareName  = 'vcbuildtools*'
+  checksum      = ''
+  checksumType  = 'md5'
+  checksum64    = ''
+  checksumType64= 'md5'
+}
+
+Install-ChocolateyPackage @packageArgs
